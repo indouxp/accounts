a43da694e7597006dcc7b8ebde3fc58ab7a3f9a4
Tatsuo Indou <indou.tsystem@gmail.com>
2011-12-20 01:26:09 +0900

    12/20

--- accounts.rb
+++ accounts.rb
@@ -7,22 +7,44 @@ require './item.rb'
 #
 #
 class Record
-  $msgs = {0=>"年月日", 1=>"科目コード", 2=>"取引金額", 3=>"適用", 4=>"支払先など"}
-  attr_accessor :item, :sum_of_money, :date, :application, :payee
+  require 'date'
+  $msgs = {0=>"年月日", 1=>"科目コード", 2=>"取引金額", 3=>"摘要", 4=>"支払先など"}
+  attr_accessor :item, :sum_of_money, :date, :summary, :payee
   def initialize
     @item = nil         # 科目
     @sum_of_money = nil # 金額
     @date = nil         # 日付
-    @application = nil  # 適用
+    @summary = nil      # 摘要
     @payee = nil        # 支払先
+  end
+
+  #
+  #
+  #
+  def complement_date(s, year = Time.now.year.to_s)
+    begin
+      ymds = s.split(/\//)
+      case ymds.size
+      when 3
+        d_result = Date.new(ymds[0].to_i, ymds[1].to_i, ymds[2].to_i)
+      when 2
+        d_result = Date.new(year.to_i, ymds[0].to_i, ymds[1].to_i)
+      else
+        raise "#{s}は日付として妥当ではありません。"
+      end
+      result = d_result.strftime("%Y/%m/%d")
+      return result
+    rescue
+      raise "#{s}は日付フォーマットではありません。"
+    end
   end
   #
   # Recordに、s_inを追加
   #
   def add(item_code, s_in)
     case
-    when s_in =~ /\d{4}?\/?\d{2}\/\d{2}/ # 年月日
-      @date = s_in
+    when s_in =~ /\d{4}?\/?\d{1,2}\/\d{1,2}/ # 年月日
+      @date = self.complement_date(s_in)
       message("日付:#{@date}")
       true
     when s_in =~ /^\d{4}$/                # 科目
@@ -46,11 +68,11 @@ class Record
     else
       result = false
       while true
-        message("#{s_in}\n> a:適用, p:支払先 c:Cancel ?:", true)
-        s_which = gets.chomp
+        message("#{s_in}\n> s:摘要, p:支払先 c:Cancel ?:", true)
+        s_which = STDIN.gets.chomp
         case s_which
-        when "a"
-          @application = s_in
+        when "s"
+          @summary = s_in
           result = true
           break
         when "p"
@@ -67,11 +89,11 @@ class Record
 end
 
 def display_rec(rec, item_code)
-  puts "日付  :Date       :#{rec.date}"
-  puts "科目  :Item       :#{rec.item}:#{item_code.check(rec.item)}" 
-  puts "金額              :#{rec.sum_of_money}"
-  puts "適用  :Application:#{rec.application}"
-  puts "支払先:Payee      :#{rec.payee}"
+  puts "日付  :Date   :#{rec.date}"
+  puts "科目  :Item   :#{rec.item}:#{item_code.check(rec.item)}" 
+  puts "金額          :#{rec.sum_of_money}"
+  puts "摘要  :Summary:#{rec.summary}"
+  puts "支払先:Payee  :#{rec.payee}"
 end
 
 def message(str, question=false)
@@ -82,7 +104,7 @@ def message(str, question=false)
   end
 end
 
-class Help
+class Item
   def initialize(item_code)
     @items = item_code.items
     @done = true
@@ -115,7 +137,6 @@ class Help
     else
       @done = false
     end
-    message("size:#{@items.size} now:#{@now} index:#{index} count:#{count} done:#{@done}")
   end
 end
 
@@ -123,7 +144,7 @@ def clear(rec)
   rec.date = nil
   rec.item = nil
   rec.sum_of_money = nil
-  rec.application = nil
+  rec.summary = nil
   rec.payee = nil
 end
 
@@ -134,7 +155,7 @@ def display_recs(recs)
 end
 
 def ok(recs, rec)
-  recs.push("#{rec.date},#{rec.item},#{rec.sum_of_money},#{rec.application},#{rec.payee}")
+  recs.push("#{rec.date},#{rec.item},#{rec.sum_of_money},#{rec.summary},#{rec.payee}")
   clear(rec)
   rec = nil
   rec = Record.new
@@ -143,36 +164,44 @@ end
 if __FILE__ == $0
   item_code = ItemCode.new("./item.txt")
   recs = []
-  help = Help.new(item_code)
-  data_file = "./accounts.dat"
+  item = Item.new(item_code)
+  if ARGV[0] != nil
+    data_file = ARGV[0]
+  else
+    data_file = "./accounts.dat"
+  end
   File.open(data_file) do |f|
     f.each do |line|
       recs.push(line.chomp)
     end
   end
   rec = Record.new
+  bef = ""
   while true
     print "> "
-    s_in = gets.chomp
+    s_in = STDIN.gets.chomp
+    s_in = bef if s_in =~ /^$/
     case
-    when s_in =~ /(^all print$|^a$)/
+    when s_in =~ /^(all print|a)$/
       display_recs(recs)
-    when s_in =~ /(^print$|^p$)/
+    when s_in =~ /^(print|p)$/
       display_rec(rec, item_code)
-    when s_in =~ /(^quit$|^q$)/
+    when s_in =~ /^(quit|q)$/
       break
-    when s_in =~ /(^help$|^h$)/
-      help.out
-    when s_in =~ /(^help \d+$|^h \d+$)/
-      help.lookup(s_in.sub(/^h\S*\s+/, ""))
-    when s_in =~ /(^ok$|^add$)/
+    when s_in =~ /^(help|h)$/
+      message("all print|a|print|p|quit|q|help|h|ok|add") 
+      item.out
+    when s_in =~ /^(help \d+|h \d+)$/
+      message("all print|a|print|p|quit|q|help|h|ok|add") 
+      item.lookup(s_in.sub(/^h\S*\s+/, ""))
+    when s_in =~ /^(ok|add)$/
       ok(recs, rec)
     else
       if ! rec.add(item_code, s_in)
         message("ERROR")
-        print
       end
     end
+    bef = s_in
   end
   display_recs(recs)
   File.open(data_file, "w") do |f|
@@ -180,5 +209,6 @@ if __FILE__ == $0
       f.puts(line)
     end
   end
+  exit(0)
 end
-exit(0)
+
